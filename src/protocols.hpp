279839d70fd8511de6ffc82c43a22611d9dc552a
Jason <150879336+JasonCodesC@users.noreply.github.com>
2025-12-18 17:05:23 -0500

    packet done

--- src/protocols.hpp
+++ src/protocols.hpp
@@ -1,12 +1,19 @@
 
+#include <cstdint>
+#include <type_traits>
 
-struct order_header {
-    uint32_t seq_num;
-    uint32_t order_id;
+enum class MsgType : uint8_t { NewLimit = 1, Cancel=2, Modify=3};
+enum class Order_Type : uint8_t { Sell = 0, Buy = 1 }; //uint8 bc may support more stuff in the future
+
+#pragma pack(push, 1)
+struct Packet {
+  uint32_t seq_num;     // for dupes
+  uint32_t order_id;    // unique id 
+  uint32_t price_tick;  // 1 => $0.01 so 10123 = $101.23
+  uint32_t qty;         // qty
+  MsgType msg_type;     // New limit, cancel or modify
+  Order_Type side;            // Sell, Buy
 };
+#pragma pack(pop)
 
-struct order_body {
-    uint32_t price;
-    float qty;
-    bool type; // 1 == buy,   0 == sell
-}
+static_assert(sizeof(Packet) == 18);
